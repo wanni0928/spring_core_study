7f9826bbc607b0740033941d479953fa941755b1
wanni0928 <chlgudwnd123@gmail.com>
2020-12-01 10:46:10 +0900

    Update ApplicationeventpublisherApplication.java

--- applicationeventpublisher/src/main/java/com/wannistudio/applicationeventpublisher/ApplicationeventpublisherApplication.java
+++ applicationeventpublisher/src/main/java/com/wannistudio/applicationeventpublisher/ApplicationeventpublisherApplication.java
@@ -2,8 +2,10 @@ package com.wannistudio.applicationeventpublisher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class ApplicationeventpublisherApplication {
 
     public static void main(String[] args) {
