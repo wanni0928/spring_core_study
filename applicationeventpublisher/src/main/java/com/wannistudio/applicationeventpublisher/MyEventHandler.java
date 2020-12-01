0d212aabcef884ff467b466414276ddbef2fec76
wanni0928 <chlgudwnd123@gmail.com>
2020-12-01 10:46:15 +0900

    Update MyEventHandler.java

--- applicationeventpublisher/src/main/java/com/wannistudio/applicationeventpublisher/MyEventHandler.java
+++ applicationeventpublisher/src/main/java/com/wannistudio/applicationeventpublisher/MyEventHandler.java
@@ -4,13 +4,15 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyEventHandler {
 
     @EventListener
-    @Order(Ordered.HIGHEST_PRECEDENCE)
+//    @Order(Ordered.HIGHEST_PRECEDENCE)
+    @Async
     public void handle(MyEvent myEvent) {
         System.out.println(Thread.currentThread().toString());
         System.out.println("이벤트 받았다. 데이터 : " + myEvent.getData());
